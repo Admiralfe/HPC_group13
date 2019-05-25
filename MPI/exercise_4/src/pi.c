51dbfe0e1e3d6d387b3157e83e81ef529f28113c
Felix Liu <felixliu@kth.se>
2019-05-25 17:14:47 +0200

    Exercise 4 fixed.

--- MPI/exercise_4/src/pi.c
+++ MPI/exercise_4/src/pi.c
@@ -1,4 +1,5 @@
 #include "pi.h"
+#define SEED 921
 
 void init_pi(int set_seed, char *outfile)
 {
@@ -23,7 +24,7 @@ void cleanup_pi()
 
 void compute_pi(int flip, int *local_count, double *answer)
 {
-	int rank, num_ranks;
+	int rank, num_ranks, i;
 	
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
@@ -33,7 +34,7 @@ void compute_pi(int flip, int *local_count, double *answer)
 	srand(SEED * rank); // Important: Multiply SEED by "rank" when you introduce MPI!
     
 	// Calculate PI following a Monte Carlo method
-	for (int iter = 0; iter < local_iters; iter++)
+	for (i = 0; i < flip; i++)
 	{
 		// Generate random (X,Y) points
 		x = (double)random() / (double)RAND_MAX;
@@ -44,30 +45,13 @@ void compute_pi(int flip, int *local_count, double *answer)
 		if (z <= 1.0)
 			(*local_count)++;
 	}
-    
-	if (rank == 0) 
+	int count;
+	MPI_Reduce(local_count, &count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+	if (rank == 0)
 	{
-		int counts[num_ranks - 1];
-		MPI_Request requests[num_ranks - 1];
-		int count = 0;
-		
-		for (int i = 1; i < num_ranks; i++)
-		{
-			MPI_Irecv(&counts[i - 1], 1, MPI_INT, i, 0, MPI_COMM_WORLD, &requests[i - 1]);
-		}
-		MPI_Waitall(num_ranks - 1, requests, MPI_STATUSES_IGNORE);
-		
-		for (int i = 0; i < num_ranks; i++) 
-		{
-			count += counts[i];
-		}
-		// Estimate Pi and display the result
-		pi = ((double)count / (double)NUM_ITER) * 4.0;
+		pi = ((double) count / (double) (flip * num_ranks)) * 4.0;
 		*answer = pi;
-	} 
-	else 
-	{
-		MPI_Send(local_count, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);	
 	}
 	return;
 }
